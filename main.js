9d1801eb41e862d8f50b71f020d75415887ecb6c
hanjim-blip <videyhanjim@gmail.com>
2026-08-11 18:51:54 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const shareBtn = document.getElementById("shareBtn");
 const shareLinks = document.getElementById("shareLinks");
 
 shareBtn.onclick = () => {
-    window.open("https://www.facebook.com/share/g/18v31sBGRk/", "_blank")
+    window.open("https://vt.tokopedia.com/t/ZS9keU1fLTgEr-kUuSP/", "_blank")
 };
 /* UNDANGAN */
 function joinWhatsAppGroup() {
-    window.open("https://vt.tokopedia.com/t/ZS9rTFgo5XAjb-wSOge/");
+    window.open("https://vt.tokopedia.com/t/ZS9keDo3M159g-GrBTb/");
 }
 
 function openFacebookPage() {
-    window.open("https://www.facebook.com/share/g/18v31sBGRk/");
+    window.open("https://vt.tokopedia.com/t/ZS9keU1fLTgEr-kUuSP/");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
